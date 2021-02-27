cb465b8d1ab14a9979096d7dc9938b90ce46eafb
Pritam Kumar <77887792+pritamkr1@users.noreply.github.com>
2021-02-27 23:32:11 +0700

    clock javascript code added to work

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-var h = document.querySelector('.hour');
-var m = document.querySelector('.minitue');
-var s = document.querySelector('.second');
-var d = document.querySelector('.show-date');
+var hour = document.querySelector('.hour');
+var mini = document.querySelector('.minitue');
+var sec = document.querySelector('.second');
+var date = document.querySelector('.show-date');
+var session = document.querySelector('.formate');
+
+function showTime(){
+    var d = new Date();
+    var getHour = d.getHours();
+    var getMini = d.getMinutes();
+    var getSec = d.getSeconds();
+    var period = 'AM'
+   // in formate of 12 hour clock
+    if(getHour == 0){
+        getHour = 12;
+        period = "AM"
+    }
+    if(getHour > 12){
+        getHour = getHour - 12;
+        period = 'PM';
+    }
+
+    getSec = getSec < 10 ? "0" + getSec : getSec
+    getHour = getHour < 10 ? "0" + getHour : getHour
+    getMini = getMini < 10 ? "0" + getMini : getMini
+
+    session.innerHTML = period;
+    hour.innerHTML = getHour;
+    mini.innerHTML = getMini;
+    sec.innerHTML = getSec;
+    setTimeout(showTime, 1000);
+    date.innerHTML = d.toDateString();
+}
+
+showTime();
